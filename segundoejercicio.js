18a949bc89d87dd5cebed99b974d616fdff05b1a
SOFTWARE <SOFTWARE@YOPDFPRCN10F707.FormacionSena.RED>
2023-03-07 11:08:23 -0500

    actualice

--- segundoejercicio.js
+++ segundoejercicio.js
@@ -1,4 +1,29 @@
 // function random(){
-    let numero = parseInt(Math.random()*100);
-    alert(numero);
-// }
+    /*let numero = parseInt(Math.random()*100);
+    alert(numero);*/
+// 
+
+
+var num1 = Math.floor(Math.random() * 99) + 1; // Genera un número aleatorio entre 1 y 99
+var num2 = Math.floor(Math.random() * 99) + 1;
+var num3 = Math.floor(Math.random() * 99) + 1;
+
+while (num1 === num2 || num1 === num3 || num2 === num3) {
+  // Si se repiten los números, se generan nuevos números hasta que sean diferentes
+  num1 = 
+  num1 =
+
+ 
+Math.floor(Math.random() * 99) + 1;
+  num2 = 
+ 
+Math.floor(Math.random() * 99) + 1;
+  num3 = 
+  num3 =
+
+ 
+Math.floor(Math.random() * 99) + 1;
+}
+
+document.write ( "sus numeros son: " + " primero " + num1, " segundo " + num2, " tercero "+  num3);
+
